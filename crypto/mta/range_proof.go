680a6bfb904300429debd0ceb6bc858a7c053b71
Pluto (9R) <pluto@ninerealms.co>
2023-08-17 07:17:23 -0400

    Add additional cmp to proof verification

--- crypto/mta/range_proof.go
+++ crypto/mta/range_proof.go
@@ -114,6 +114,28 @@ func (pf *RangeProofAlice) Verify(pk *paillier.PublicKey, NTilde, h1, h2, c *big
 	q3 := new(big.Int).Mul(q, q)
 	q3.Mul(q3, q)
 
+	if !common.IsInInterval(pf.Z, NTilde) {
+		return false
+	}
+	if !common.IsInInterval(pf.U, pk.NSquare()) {
+		return false
+	}
+	if !common.IsInInterval(pf.W, NTilde) {
+		return false
+	}
+	if !common.IsInInterval(pf.S, pk.N) {
+		return false
+	}
+	if new(big.Int).GCD(nil, nil, pf.Z, NTilde).Cmp(one) != 0 {
+		return false
+	}
+	if new(big.Int).GCD(nil, nil, pf.U, pk.NSquare()).Cmp(one) != 0 {
+		return false
+	}
+	if new(big.Int).GCD(nil, nil, pf.W, NTilde).Cmp(one) != 0 {
+		return false
+	}
+
 	// 3.
 	if pf.S1.Cmp(q3) == 1 {
 		return false
